15f3c909c189929d15a8b9041e9c6d5f8bec40eb
Nikolay <m.nikolay1990@gmail.com>
2023-01-05 14:08:27 +0700

    added localStorage

--- src/components/layout/MainNavigation.jsx
+++ src/components/layout/MainNavigation.jsx
@@ -1,7 +1,14 @@
+import { useContext } from 'react';
+
 import { Link } from 'react-router-dom';
 import classes from './MainNavigation.module.css';
+import { FavoritesContext } from 'store/favorites-context';
 
 export const MainNavigation = () => {
+	const favoritesCtx = useContext(FavoritesContext);
+
+	const localStorageItems = JSON.parse(localStorage.getItem('userFavorites'));
+
 	return (
 		<header className={classes.header}>
 			<div className={classes.logo}>React Meetups</div>
@@ -14,7 +21,15 @@ export const MainNavigation = () => {
 						<Link to={'/new-meetup'}>New Meetup</Link>
 					</li>
 					<li>
-						<Link to={'/favorites'}>Favorites</Link>
+						<Link to={'/favorites'}>
+							My Favorites
+							{localStorageItems &&
+							localStorageItems.length > 0 ? (
+								<span className={classes.badge}>
+									{localStorageItems.length}
+								</span>
+							) : null}
+						</Link>
 					</li>
 				</ul>
 			</nav>
